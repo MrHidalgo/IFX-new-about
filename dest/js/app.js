21f7404f876b9b58bac1c4018685338a53caf7c7
MrHidalgo <Luckyhidalgo@gmail.com>
2019-02-20 16:49:27 +0200

    Team section - full adaptive.

--- dest/js/app.js
+++ dest/js/app.js
@@ -56,75 +56,35 @@ var initSwiper = function initSwiper() {
     loop: true,
     watchOverflow: true,
     normalizeSlideIndex: true,
-    grabCursor: true,
+    grabCursor: false,
     freeMode: false,
-    effect: 'slide', // "slide", "fade", "cube", "coverflow" or "flip"
-    // autoplay: {
-    //   delay: 5000,
-    // },
-    // Disable preloading of all images
-    // preloadImages: false,
-    // Enable lazy loading
-    // lazy: {
-    //   loadPrevNext: true,
-    // },
+    effect: 'slide',
 
     // off touch for destop
-    // touchMoveStopPropagation:false,
-    // simulateTouch : false,
-    // allowSwipeToNext: true,
-    // allowSwipeToPrev: true,
-    // allowPageScroll: "auto ",
+    touchMoveStopPropagation: false,
+    simulateTouch: false,
+    allowSwipeToNext: true,
+    allowSwipeToPrev: true,
+    allowPageScroll: "auto ",
 
     slidesPerView: 4,
     spaceBetween: 30,
     breakpoints: {
-      // when window width is <= 320px
-      320: {
-        // slidesPerView: 1,
-        // spaceBetween: 10
+      575: {
+        slidesPerView: 1
       },
-      // when window width is <= 480px
-      480: {
-        // slidesPerView: 2,
-        // spaceBetween: 20
+      991: {
+        slidesPerView: 2
       },
-      // when window width is <= 640px
-      640: {
-        // slidesPerView: 3,
-        // spaceBetween: 30
+      1199: {
+        slidesPerView: 3
       }
     },
 
-    // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    //   clickable: true,
-    //   // renderBullet: function (index, className) {
-    //   //   return `
-    //   //     <div class="${className}">
-    //   //       ${index}
-    //   //     </div>
-    //   //   `;
-    //   // }
-    // },
-
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
     }
-
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
-
-    // on: {
-    //   "slideChange": function () {
-    //     console.log("slideChange");
-    //   },
-    // }
   });
 };
 
